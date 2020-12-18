2488bd06a5c552ecb3927405203ce16beab48fcb
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2020-12-18 14:37:56 -0600

    completed for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,4 +1,5 @@
-function showMultiplicationTable(input){
+//2
+ function showMultiplicationTable(input){
     for (var x = 1; x < 10; x++){
         console.log((input + '*' + x + '=' + (input * x)))
     }
@@ -13,3 +14,23 @@ showMultiplicationTable(7);
 showMultiplicationTable(8);
 showMultiplicationTable(9);
 showMultiplicationTable(10);
+
+//3
+for ( i = 1; i < 10; i++){
+   var randomNumber = Math.floor(Math.random() * 180) + 20;
+    if (randomNumber % 2 === 0){
+       console.log( randomNumber + ' is an even number.');
+    } else {
+       console.log( randomNumber + ' is an odd number.');
+    }
+}
+
+//4
+for (var i = 1; i <= 9; i++){
+   console.log(i.toString().repeat(i));
+}
+
+//5
+for (i = 100; i >= 5; i -=5){
+    console.log(i)
+}
