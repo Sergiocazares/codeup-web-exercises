35e5370f5a10dab321d07ac6beab1904f7f77500
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2020-12-18 14:04:06 -0600

    feat: question 2 finished

---
+++ js/for_loops.js
@@ -0,0 +1,15 @@
+function showMultiplicationTable(input){
+    for (var x = 1; x < 10; x++){
+        console.log((input + '*' + x + '=' + (input * x)))
+    }
+}
+showMultiplicationTable(1);
+showMultiplicationTable(2);
+showMultiplicationTable(3);
+showMultiplicationTable(4);
+showMultiplicationTable(5);
+showMultiplicationTable(6);
+showMultiplicationTable(7);
+showMultiplicationTable(8);
+showMultiplicationTable(9);
+showMultiplicationTable(10);
