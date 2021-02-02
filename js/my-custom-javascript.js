fca6a0373bdfd6631e6e129814ae6cc772f5ecd9
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-02-02 13:38:22 -0600

    feat: completed selectors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,3 +1,12 @@
 $(document).ready(function (){
+    var idContents = $('#container').html();
+    alert(idContents);
+    $('.codeup').css("border","red solid 1px");
+    $('li').css("font-size","20px");
+
+    $('h1 , li , p').css("color", "purple");
+
+    var h1Contents = $("h1").html();
+    alert(h1Contents);
     alert("Dom Ready!")
 })
