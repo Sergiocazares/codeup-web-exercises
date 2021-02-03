8ee35f542f53cc553d87eb3e0cab6d8ff0c7c0db
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-02-03 10:06:45 -0600

    feat: completed mouse events

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,12 +1,32 @@
 $(document).ready(function (){
-    var idContents = $('#container').html();
-    alert(idContents);
-    $('.codeup').css("border","red solid 1px");
-    $('li').css("font-size","20px");
+    // var idContents = $('#container').html();
+    // alert(idContents);
+    // $('.codeup').css("border","red solid 1px");
+    // $('li').css("font-size","20px");
+    //
+    // $('h1 , li , p').css("color", "purple");
+    //
+    // var h1Contents = $("h1").html();
+    // alert(h1Contents);
+    // alert("Dom Ready!")
 
-    $('h1 , li , p').css("color", "purple");
+    $("h1").click(function(){
+        $(this).css("background-color", "red");
+    });
 
-    var h1Contents = $("h1").html();
-    alert(h1Contents);
-    alert("Dom Ready!")
-})
+    $("p").dblclick(function(){
+        $(this).css("font-size", "18px");
+    })
+
+    $("li").hover(
+        function () {
+            $(this).css("color","red");
+        },
+        function () {
+            $(this).css("color","black");
+        }
+
+
+    );
+});
+
