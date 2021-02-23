e8677d6aa35044be4da657c6efd9be37490441be
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-02-23 15:56:33 -0600

    completed

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -37,3 +37,39 @@ const users = [
 ];
 
 
+const threeLanguages = users.filter((user) => user.languages.length >= 3);
+console.log(threeLanguages);
+
+const usersEmails = users.map((user) =>  user.email);
+console.log(usersEmails)
+
+const totalYearsExperience = users.reduce((total, user) => {
+    return total + user.yearsOfExperience;
+}, 0);
+
+console.log("Total years of experience: " , totalYearsExperience);
+
+const averageYearsExperience = totalYearsExperience / users.length
+
+console.log(averageYearsExperience);
+
+const longestEmail = users.reduce((longest, user) => {
+    if (user.email.length > longest.length) {
+        return user.email
+    } else {
+        return longest;
+    }
+}, "");
+
+console.log(longestEmail);
+
+const nameList = users.reduce((str, user, index) => {
+    if(index === 0) {
+        return user.name;
+    } else {
+        return str + ", " + user.name;
+    }
+
+}, "");
+
+console.log(nameList);
